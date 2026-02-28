7ace828e59c1d984f4b87cfa7e3fa8d49a5052a9
Jonathan Behrens <fintelia@gmail.com>
2026-02-27 17:37:39 -0800

    Remove thiserror dependency

--- src/data/block.rs
+++ src/data/block.rs
@@ -1,6 +1,4 @@
-use std::{fmt, fmt::Display};
-
-use thiserror::Error;
+use std::{error::Error, fmt::{self, Display}};
 
 use crate::data::{
     plane::PlaneOffset,
@@ -255,8 +253,10 @@ impl BlockSize {
     }
 }
 
-#[derive(Debug, Copy, Clone, Error, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct InvalidBlockSize;
+
+impl Error for InvalidBlockSize {}
 
 impl Display for InvalidBlockSize {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
